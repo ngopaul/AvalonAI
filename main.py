a202f2a4446c89d6ff1906de6eb05de7f90442d7
briancnguyen <briancnguyen@berkeley.edu>
2018-08-08 01:22:22 -0700

    Changed to OOP and Configured Database Access

--- main.py
+++ main.py
@@ -2,146 +2,163 @@
 import sqlite3
 from sqlite3 import OperationalError
 
-""" General global unchanging variables """
-game_state = 3
-# Game states:
-# 0 : Minions of Mordred win
-# 1 : Servants of Arthur win
-# 2 : Servants of Arthur won, but Minions of Mordred still choose to assassinate
-# 3 : Game in progress
-num_players = 0
-# the number of players playing
-role_types = [0, 0, 0, 0, 0, 0, 0]
-# number of each role, in this order per index:
-# 0: normal bad (assassin counts as normal bad)
-# 1: normal good
-# 2: Merlin
-# 3: Percival
-# 4: Morgana
-# 5: Mordred
-# 6: Oberon
+class Avalon:
+    def __init__(self):
+        """ Game States:
+            # 0 : Minions of Mordred win
+            # 1 : Servants of Arthur win
+            # 2 : Servants of Arthur won, but Minions of Mordred still choose to assassinate
+            # 3 : Game in progress """
+        self.game_state = 3
+        """ Number of Players:
+            # The number of players playing """
+        self.num_players = 0
+        """ Role Types:
+            Number of each role, in this order per index:
+            # 0: normal bad (assassin counts as normal bad)
+            # 1: normal good
+            # 2: Merlin
+            # 3: Percival
+            # 4: Morgana
+            # 5: Mordred
+            # 6: Oberon """
+        self.role_types = [0, 0, 0, 0, 0, 0, 0]
 
-""" Varying variables that change throughout the game """
-current_leader = 0
-# the player ID of the current leader.
-quest_state = [-1, -1, -1, -1, -1, 0]
-# the first five represent fails or successes.
-# 0 is fail, 1 is success
-# the last entry represents how many rejected quests in a row there are.
-people_per_quest = [0, 0, 0, 0, 0]
-# the number of people on each quest
-propose_history = []
-# list of proposals. Each proposal is a list of people in the proposal. Some proposals may not even
-# make it to the voting stage; all this means is that VOTE_HISTORY is empty at that index.
-vote_history = []
-# history of votes. Each entry is a list of votes, 0 or 1, based on the person's (index in the list) vote.
-# This should be the same length as propose history.
-quest_history = []
-# history of quests. Each entry is a list, with these values at each index:
-# 0: the position in PROPOSE_HISTORY that this quest was proposed (same as the position in VOTE_HISTORY)
-# 1: fail or success? (0 and 1 respectively)
-# 2: number of fails
-# 3: number of successes
-known_players = []
-# list of size NUM_PLAYERS, where each index correlates to the player number
-# the values are the role types, as above.
+        """ Varying variables that change throughout the game """
 
-""" Database methods """
-import sqlite3
-conn = sqlite3.connect('avalon.db')
-c = conn.cursor()
+        """ Current Leader: 
+            # The player ID of the current leader. """
+        self.current_leader = 0
+        """ Quest State: 
+            The first five represent fails or successes.
+            # 0 is fail, 1 is success
+            # the last entry represents how many rejected quests in a row there are. """
+        self.quest_state = [-1, -1, -1, -1, -1, 0]
+        """ People per Quest: 
+            # The number of people on each quest """
+        self.people_per_quest = [0, 0, 0, 0, 0]
+        """ Propose History:
+            # List of proposals. Each proposal is a list of people in the proposal. Some proposals may not even
+            # Make it to the voting stage; all this means is that VOTE_HISTORY is empty at that index. """
+        self.propose_history = []
+        """ Voting History: 
+            # history of votes. Each entry is a list of votes, 0 or 1, based on the person's (index in the list) vote.
+            # This should be the same length as propose history. """
+        self.vote_history = []
+        """ Quest History:
+            # History of quests. Each entry is a list, with these values at each index:
+            # 0: the position in PROPOSE_HISTORY that this quest was proposed (same as the position in VOTE_HISTORY)
+            # 1: fail or success? (0 and 1 respectively)
+            # 2: number of fails
+            # 3: number of successes """
+        self.quest_history = []
+        """ Known Players:  
+            # list of size NUM_PLAYERS, where each index correlates to the player number
+            # the values are the role types, as above. """
+        self.known_players = []
 
-""" Allows us to read in SQL files. """
-def executeScriptsFromFile(filename):
-    # Open and read the file as a single buffer
-    fd = open(filename, 'r')
-    sqlFile = fd.read()
-    fd.close()
+        self.initialize()
+       
 
-    # all SQL commands (split on ';')
-    sqlCommands = sqlFile.split(';')
+    """ Allows us to read in SQL files. """
+    def executeScriptsFromFile(self, filename):
+        # Open and read the file as a single buffer
+        fd = open(filename, 'r')
+        sqlFile = fd.read()
+        fd.close()
 
-    # Execute every command from the input file
-    for command in sqlCommands:
-        # This will skip and report errors
-        # For example, if the tables do not yet exist, this will skip over
-        # the DROP TABLE commands
-        try:
-            c.execute(command)
-        except OperationalError as msg:
-            print("Command skipped: ", msg)
+        # all SQL commands (split on ';')
+        sqlCommands = sqlFile.split(';')
 
-""" Initializes the game. """
-def initialize():
-    global num_players, role_types, people_per_quest, current_leader, known_players
-    num_players = int(input("Number of players: "))
-    role_types[0] = int(input("Normal Bad: "))
-    role_types[1] = int(input("Normal Good: "))
-    role_types[2] = int(input("Merlin: "))
-    role_types[3] = int(input("Percival: "))
-    role_types[4] = int(input("Morgana: "))
-    role_types[5] = int(input("Mordred: "))
-    role_types[6] = int(input("Oberon: "))
-    print("Initializing databases...")
-    executeScriptsFromFile("avalon.sql")
-    # TODO
+        # Execute every command from the input file
+        for command in sqlCommands:
+            # This will skip and report errors
+            # For example, if the tables do not yet exist, this will skip over
+            # the DROP TABLE commands
+            try:
+                c.execute(command)
+            except OperationalError as msg:
+                print("Command skipped: ", msg)
 
-""" One player accuses another of being evil, or being a specific role. """
-def accuse():
-    # TODO
-    pass
+    """ Initializes the game. """
+    def initialize(self):
+        self.num_players = int(input("Number of players: "))
+        self.role_types[0] = int(input("Normal Bad: "))
+        self.role_types[1] = int(input("Normal Good: "))
+        self.role_types[2] = int(input("Merlin: "))
+        self.role_types[3] = int(input("Percival: "))
+        self.role_types[4] = int(input("Morgana: "))
+        self.role_types[5] = int(input("Mordred: "))
+        self.role_types[6] = int(input("Oberon: "))
+        print("Initializing databases...")
+        self.executeScriptsFromFile("avalon.sql")
+        self.query_database("avalon.db")
 
-""" The person using the AI knows the alignment of a player. """
-def known():
-    known_players[input("Which person? ")] = input("Which role? ")
-
-""" The current leader proposes a team. """
-def propose_team():
-    # TODO
-    pass
-
-""" The players vote on the most recently proposed team. If rejected, adds to the rejected tally. """
-def vote():
-    # TODO
-    pass
-
-""" Gets the results of a quest. Passes possesion of the leader to the next person. """
-def quest():
-    # TODO
-    global current_leader
-    current_leader = (current_leader + 1) % num_players
-
-def print_help():
-    # TODO
-    pass
+    def query_database(self, db_file):
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM player_alignment")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
 
+    """ One player accuses another of being evil, or being a specific role. """
+    def accuse(self):
+        # TODO
+        pass
 
-""" Heuristics for Minions of Mordred 
+    """ The person using the AI knows the alignment of a player. """
+    def known(self):
+        known_players[input("Which person? ")] = input("Which role? ")
 
-Please write out the strategies that you think are valid for Minions, i.e. what would a minion do?
+    """ The current leader proposes a team. """
+    def propose_team(self):
+        # TODO
+        pass
 
-"""
+    """ The players vote on the most recently proposed team. If rejected, adds to the rejected tally. """
+    def vote(self):
+        # TODO
+        pass
 
-# Vote through an to-fail quest
-def mm_vote_to_fail():
-    pass
+    """ Gets the results of a quest. Passes possesion of the leader to the next person. """
+    def quest(self):
+        # TODO
+        global current_leader
+        current_leader = (current_leader + 1) % num_players
 
-""" Heuristics for Servants of Arthur 
-
-Please write out the strategies that you think are valid for Servants, i.e. what would a servant do?
-
-"""
-
-# Vote against a to-fail quest
-def sa_vote_against_fail():
-    pass
+    def print_help(self):
+        # TODO
+        pass
 
 
-initialize()
+
+    """ Heuristics for Minions of Mordred 
+
+    Please write out the strategies that you think are valid for Minions, i.e. what would a minion do?
+
+    """
+
+    # Vote through an to-fail quest
+    def mm_vote_to_fail(self):
+        pass
+
+    """ Heuristics for Servants of Arthur 
+
+    Please write out the strategies that you think are valid for Servants, i.e. what would a servant do?
+
+    """
+
+    # Vote against a to-fail quest
+    def sa_vote_against_fail(self):
+        pass
+
+
+a = Avalon()
 user_input = ""
 
-while (game_state > 1):
+while (a.game_state > 1):
     user_input = input("Command (type help for commands): ")
     if (user_input == "help"):
         print_help()
